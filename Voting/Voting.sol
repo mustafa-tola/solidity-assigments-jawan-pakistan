97ca4661382c5c0ab0025f70a1e100f40c7bf10a
mustafa-tola <mustafatola02@gmail.com>
2022-09-08 11:01:02 -0700

    Added voting task

---
+++ Voting/Voting.sol
@@ -0,0 +1,39 @@
+pragma solidity ^0.8.16;
+
+contract Voting {
+    mapping(address => bool) blacklistedVoters;
+    address owner;
+    mapping (string => uint) parties;
+
+    constructor() {
+        owner = msg.sender;
+        parties["PTI"] = 0;
+        parties["PMLN"] = 0;
+        parties["PPP"] = 0;
+    }
+
+    function setVoterStatus(address voter,bool status) public {
+        require(owner == msg.sender,"You are not an admin");
+        blacklistedVoters[voter] = status;
+    }
+
+    function votePti() public {
+        require(blacklistedVoters[msg.sender] == true,"You can not vote");
+        parties["PTI"] += 1;
+    }
+
+    function votePmln() public {
+        require(blacklistedVoters[msg.sender] == false,"You can not vote");
+        parties["PMLN"] += 1;
+    }
+
+    function votePpp() public {
+        require(blacklistedVoters[msg.sender] == false,"You can not vote");
+        parties["PPP"] += 1;
+    }
+
+    function getVotesStatus() public view returns (uint,uint,uint) {
+        require(owner == msg.sender,"You are not an admin");
+        return (parties["PTI"],parties["PMLN"],parties["PPP"]);
+    }
+}
