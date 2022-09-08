e9ea02b31a43e50355feb8f498e2de13efb2a776
mustafa-tola <mustafatola02@gmail.com>
2022-09-08 11:23:03 -0700

    Added array sort task

---
+++ ArraySort/ArraySort.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
+
+contract ArraySort {
+   function sort(uint[] memory array,int length) public pure returns(uint[] memory) {
+        uint temp;
+        uint temp1;
+        uint ind;
+        for(int i = 0;i < length;i++) {
+            temp = uint(array[i]);
+            ind = i;
+            for(int j = i + 1;j < length;j++) {
+                if(array[j] < temp) {
+                    temp = uint(array[j]);
+                    ind = j;
+                }
+            }
+            temp1 = array[uint256(i)];
+            array[uint(i)] = temp;
+            array[uint(ind)] = temp1;
+        }
+        return array;
+   }
+}
